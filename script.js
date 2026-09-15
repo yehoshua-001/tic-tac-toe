f887e5ca062e8a24abf45e1abb4ad7dec5fd0d7b
yehoshua-001 <joshuafelicilda248@gmail.com>
2026-09-15 22:17:38 +0800

    Edit script.js
    Added closures that checks the cells if empty, createPlayer() for
    'players' array, switchTurn closure that switches 'activePlayer' value,
    and playRound closure. And changed some codes.

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ const Gameboard = ( function() {
     for (let r = 0; r < rows; r++) {
         board[r] = [];
         for (let c = 0; c < columns; c++) {
-            board[r][c] = "";
+            board[r][c] = null;
         }
     };
 
@@ -21,38 +21,103 @@ const Gameboard = ( function() {
         [2, 5, 8],
         [0, 4, 8], // diagonals
         [2, 4, 6]
-    ];    
+    ];
 
-    const placeMark = (row, column) => {
-        let mark = "X"
-        board[row][column] = mark;
-        printBoard();
-        GameController.inputNum();
+    const isCellValid = (row, column) =>
+        Number.isInteger(row) && 
+        Number.isInteger(column) &&
+        row >= 0 && 
+        row < rows &&
+        column >= 0 && 
+        column < columns;
+
+    const isCellEmpty = (row, column) =>
+        isCellValid(row, column) && board[row][column] === null;
+
+    const placeMark = (row, column, mark) => {
+        if (!isCellEmpty(row, column)) {
+            console.log('Cell is occupied');
+            return false;
+        }
+        else {
+            board[row][column] = mark;
+            printBoard();
+            return true;
+        }
     };
 
     const printBoard = () => {
         console.table(board);
-        console.log(board.length)
     }; 
 
     return {
-        printBoard,
+        isCellEmpty,
         placeMark,
+        printBoard,
     };
 })();
 
+function createPlayer(defaultName, mark) {
+    let name = defaultName;
+    
+    const getName = () => name;
+
+    const setName = (value) => {
+        name = String(value ?? "").trim() || defaultName;
+    };
+
+    return {
+        getName,
+        setName,
+        getMark: () => mark,
+    };
+}
+
 const GameController = ( function() {
+    const players = [
+        createPlayer('Player 1', 'X'),
+        createPlayer('Player 2', 'O'),
+    ];
+
+    let index = 0;
+    let activePlayer = players[index];
+
+    const switchTurn = () => {
+        activePlayer = activePlayer === players[0] ? players[1] : players[0];
+    };
+
     // This is for console mode
     const inputNum = () => {
-        let input = prompt("Row, Column");
+        let input = prompt(`${activePlayer.getName()}'s turn ${activePlayer.getMark()} (row, column)`);
         const row = Number(input.split(",")[0]);
         const column = Number(input.split(",")[1]);
-        Gameboard.placeMark(row, column);
+        playRound(row, column);
+    };
+
+    const playRound = (row, column) => {
+        const mark = activePlayer.getMark();
+        const move = Gameboard.placeMark(row, column, mark);
+
+        if (!move) {
+            inputNum();
+            return;
+        };
+
+        switchTurn();
+        inputNum();
+
+        return {
+            row,
+            column,
+            mark,
+        };
     };
     
     return {
-        inputNum
+        inputNum,
+        playRound,
     };
 })();
 
+Gameboard.printBoard();
 GameController.inputNum();
